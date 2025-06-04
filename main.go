6369fc115ad22e1717603da0432344a81a7799b8
fribeir0 <felipecarv971@gmail.com>
2025-06-03 21:56:37 -0300

    <feat>mvp 0.01

--- main.go
+++ main.go
@@ -3,10 +3,26 @@ package main
 import (
     "github.com/gin-gonic/gin"
     "go-recon-ai-modular/internal/handlers"
+    "os"
+    "log"
 )
 
 func main() {
     r := gin.Default()
+
+    r.GET("/", func(c *gin.Context) {
+        c.File("./web/index.html")
+    })
+
     r.POST("/recon", handlers.ReconHandler)
-    r.Run(":8080")
+
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
+    log.Printf("Iniciando servidor na porta %s...", port)
+
+    r.Run(":" + port)
 }
+
+
