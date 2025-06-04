82b5ce2805e5f38399f44db117c86dcecb9b4b92
fribeir0 <felipecarv971@gmail.com>
2025-06-03 21:18:37 -0300

    <feat>mvp v0.991 speed

--- main.go
+++ main.go
@@ -1,20 +1,26 @@
 package main
 
 import (
-    "log"
-    "os"
-
     "github.com/gin-gonic/gin"
     "go-recon-masscan-nmap-universal/internal/handlers"
+    "log"
+    "os"
 )
 
 func main() {
+    r := gin.Default()
+
+    r.GET("/", func(c *gin.Context) {
+        c.File("./web/index.html")
+    })
+
+    r.POST("/recon", handlers.ReconHandler)
+
     port := os.Getenv("PORT")
     if port == "" {
         port = "8080"
     }
-    log.Printf("Starting server on port %s", port)
-    r := gin.Default()
-    r.POST("/recon", handlers.ReconHandler)
+    log.Printf("Iniciando servidor na porta %s...", port)
+
     r.Run(":" + port)
 }
